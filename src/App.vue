ad73cc8c11a6af3fb737496de502fbcc35e7c1df
Aleksey Pechurin <pe4urin@yandex.ru>
2023-05-13 15:23:38 +0300

    работа с localStorage, рефакторинг запросов

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,25 @@
   <div
     class="text-xs sm:text-sm lg:text-base text-gray-100 font-main main-bg-day"
   >
-    <Loader v-if="loading" />
+              <!-- preview -->
+    <div class="absolute top-[30%] left-[50%] translate-x-[-50%] bg-block icon__02d icon-preview py-24 px-2 w-2/3 md:w-1/2
+    " v-if="hello && !weatherInfo">
+      <p class="text-xl text-center">Введите город, чтобы узнать прогноз погоды</p>
+    </div>
+    <Loader v-if="!weatherInfo && !hello" />
     <div class="min-h-screen xl:container mx-auto p-4 sm:p-6 md:p-8 lg:p-8 xl:p-40 grid grid-rows-[40px_1fr_1fr_1fr] xl:grid-cols-2 xl:grid-rows-[40px_1fr_1fr] gap-4 md:gap-6">
-      <!-- <Search/> -->
+      <!-- <search> -->
       <div class="col-span-full pr-3 flex bg-block">
         <input
           class="bg-sky-200 text-black rounded-3xl h-10 w-11/12 px-8 text-lg focus:outline-sky-400 hover:outline hover:outline-sky-500 hover:outline-2 z-10 capitalize"
           type="text"
           v-model="city"
-          @keydown.enter="getWeather"
+          @keydown.enter="handleSubmit"
         />
         <button
           class="absolute left-[100%] translate-x-[-100%] w-1/3 h-10 pr-2 md:pr-6 rounded-3xl bg-sky-600 focus:outline-none hover:bg-sky-700 transition-all"
           type="button"
-          @click="getWeather"
+          @click="handleSubmit"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -35,7 +40,7 @@
         </button>
       </div>
       <!-- error -->
-      <div class="w-fit h-10 px-5 pt-2  bg-block" v-if="isError && !loading">
+      <div class="w-fit h-10 px-5 pt-2  bg-block" v-if="isError && weatherInfo">
         <p>Кажется, что то пошло не так, попробуйте снова</p>
       </div>
       <City :city="city" :weatherInfo="weatherInfo" v-if="!isError"/>
@@ -46,37 +51,41 @@
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
+import { computed, ref, } from "vue";
 import { API_KEY, BASE_URL, LIST_URL } from "@/constants";
+import { setLocalWeather, setLocalWeatherList, getLocalWeatherAll } from '@/utils'
 import City from "@/components/TheCity.vue";
 import Hours from "@/components/TheHours.vue";
 import Current from "@/components/TheCurrent.vue";
 import Loader from "@/components/UI/TheLoader.vue";
 
-const city = ref("Belgorod");
+const hello = ref(true);
+const city = ref("");
 const weatherInfo = ref(null);
-
-function gerWeatherInfo() {
-  fetch(`${BASE_URL}?q=${city.value}&appid=${API_KEY}&units=metric&lang=ru`)
-    .then((response) => response.json())
-    .then((data) => (weatherInfo.value = data))
-    .then(() => (city.value = ""));
-}
 const weatherList = ref(null);
 
-function getWeatherList() {
-  fetch(`${LIST_URL}?q=${city.value}&appid=${API_KEY}&units=metric&cnt=6`)
-    .then((response) => response.json())
-    .then((data) => (weatherList.value = data));
-}
+getLocalWeatherAll(weatherInfo, weatherList)
 
 function getWeather() {
-  gerWeatherInfo();
-  getWeatherList();
-}
-getWeather();
+  fetch(`${BASE_URL}?q=${city.value}&appid=${API_KEY}&units=metric&lang=ru`)
+    .then((response) => response.json())
+    .then((data) => weatherInfo.value = data)
+    .then((data)=> setLocalWeather(data));
 
-const loading = computed(() => !weatherInfo.value);
+  fetch(`${LIST_URL}?q=${city.value}&appid=${API_KEY}&units=metric&cnt=6`)
+    .then((response) => response.json())
+    .then((data) => weatherList.value = data)
+    .then((data)=> setLocalWeatherList(data));
+}
+
+function handleSubmit() {
+  if (city.value) {
+    hello.value = false
+    getWeather()
+    city.value = ""
+  }
+}
+
 const isError = computed(
   () => weatherInfo.value?.cod !== 200 && weatherList.value?.cod !== 200
 );
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,14 +2,22 @@ const PRESSURE_UNITS = 0.757762
 
 export const convertPressure = (hpa) => Math.round(hpa * PRESSURE_UNITS)
 
-function convertTimes(dateTime) {
+export const convertTime = (dateTime) => {
   const date = new Date(dateTime * 1000)
   const hours = date.getHours()
   return hours
 }
 
-export const convertTime = (dateTime) => {
-  const date = new Date(dateTime * 1000)
-  const hours = date.getHours()
-  return hours
+export const setLocalWeather = data =>  localStorage.setItem('localWeather', JSON.stringify(data))
+
+export const setLocalWeatherList = data => localStorage.setItem('localWeatherList', JSON.stringify(data))
+
+export const getLocalWeatherAll = (weatherInfo, weatherList) => {
+  const localWeather = localStorage.getItem('localWeather')
+  const localWeatherList = localStorage.getItem('localWeatherList')
+
+  if (JSON.parse(localWeather) && JSON.parse(localWeatherList)) {
+    weatherInfo.value = JSON.parse(localWeather)
+    weatherList.value = JSON.parse(localWeatherList)
+  }
 }
